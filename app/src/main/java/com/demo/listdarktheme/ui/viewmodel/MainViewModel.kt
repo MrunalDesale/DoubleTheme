39f45b63fef49a52966b5f3001e7514890bb272b
Desale <2079610@cognizant.com>
2021-12-09 16:20:39 +0530

    Error message handling

--- app/src/main/java/com/demo/listdarktheme/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/demo/listdarktheme/ui/viewmodel/MainViewModel.kt
@@ -3,15 +3,17 @@ package com.demo.listdarktheme.ui.viewmodel
 import android.app.Application
 import androidx.lifecycle.*
 import androidx.lifecycle.AndroidViewModel
+import com.demo.listdarktheme.rest.model.ErrorResponse
 import com.demo.listdarktheme.rest.model.ResultWrapper
 import com.demo.listdarktheme.rest.repository.RecipeRepository
 import com.demo.listdarktheme.rest.utils.NetworkUtils
 import com.demo.listdarktheme.ui.model.RecipeModel
 import kotlinx.coroutines.launch
 
-open class MainViewModel(application: Application,private val recipeRepository: RecipeRepository) :
+open class MainViewModel(application: Application, private val recipeRepository: RecipeRepository) :
     AndroidViewModel(application) {
     private val mRecipesResult: MutableLiveData<List<RecipeModel>?> = MutableLiveData()
+    private val mError: MutableLiveData<ErrorResponse> = MutableLiveData()
 
     fun getRecipes(pageNo: Int, size: Int) = viewModelScope.launch {
         val recipeList = NetworkUtils.makeApiCall {
@@ -19,10 +21,10 @@ open class MainViewModel(application: Application,private val recipeRepository: 
         }
         when (recipeList) {
             is ResultWrapper.NetworkError -> {
-
+                mError.postValue(ErrorResponse("Network error"))
             }
             is ResultWrapper.GenericError -> {
-
+                mError.postValue(recipeList.error ?: ErrorResponse("Something went wrong. Please try again."))
             }
 
             is ResultWrapper.Success -> {
@@ -32,6 +34,7 @@ open class MainViewModel(application: Application,private val recipeRepository: 
     }
 
     fun getRecipesList() = mRecipesResult
+    fun getError() = mError
 
     val recipeModelList: LiveData<List<RecipeModel?>?> = recipeRepository.recipeList.asLiveData()
 
@@ -39,11 +42,14 @@ open class MainViewModel(application: Application,private val recipeRepository: 
         recipeRepository.insertRecipe(recipeModel)
     }
 
-    class MainViewModelFactory(private val application: Application,private val repository: RecipeRepository) : ViewModelProvider.Factory {
+    class MainViewModelFactory(
+        private val application: Application,
+        private val repository: RecipeRepository
+    ) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return MainViewModel(application ,repository) as T
+                return MainViewModel(application, repository) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
         }
